9be5a939e9aa11d67dea597e4220912d3babc29d
Nico Kurniawan <nico.kurniawan@gdn-commerce.com>
2020-03-15 21:17:06 +0700

    remove notes in demoApp

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,8 @@
 package com.example.demo;
 
-import java.util.function.Consumer;
-
 public class DemoApplication {
 
-	private static void toLowerCaseAndThenPrint(String data) {
-		data = data.toLowerCase();
-		System.out.println(data);
-	}
-
 	public static void main(String[] args) {
-		String name = "Nico Adhitya Kurniawan";
 
-		Consumer<String> printConsumer1 =
-				value -> System.out.println(value);
-		Consumer<String> printConsumer2 = System.out::println;
-		Consumer<String> consumer3 =
-				value -> {
-					value = value.toLowerCase();
-					System.out.println(value);
-				};
-		Consumer<String> consumer4 = DemoApplication::toLowerCaseAndThenPrint;
-
-		consumer4.accept(name);
 	}
 }
