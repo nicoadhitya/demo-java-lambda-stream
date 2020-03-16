4039a537a40ee24ec33534339d5e78d27e708d4f
Nico Kurniawan <nico.kurniawan@gdn-commerce.com>
2020-03-16 11:04:04 +0700

    demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,39 @@
 package com.example.demo;
 
+import java.util.function.Consumer;
+
 public class DemoApplication {
+
+  private static void print(String input) {
+    System.out.println(input);
+  }
+
+  private static void toLowerCaseThenPrint(String input) {
+    input = "Mr. " + input;
+    input = input.toLowerCase();
+    System.out.println(input);
+  }
 
 	public static void main(String[] args) {
 
+	  String name = "John";
+	  Integer score = 10;
+
+		Consumer<String> printConsumer =
+        value -> {
+          value = "Mr. " + value;
+          value = value.toLowerCase();
+          System.out.println(value);
+        };
+
+    Consumer<String> printConsumerWithFunction =
+        value -> toLowerCaseThenPrint(value);
+
+    Consumer<String> printConsumerWithFunctionWithReference =
+        DemoApplication::toLowerCaseThenPrint;
+
+    printConsumer.accept(name);
+    printConsumerWithFunction.accept(name);
+    printConsumerWithFunctionWithReference.accept(name);
 	}
 }
